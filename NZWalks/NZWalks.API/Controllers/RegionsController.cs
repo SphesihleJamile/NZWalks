784ca336f81d7bd23e73913e5b627ad3bce30cd4
Sphesihle Jamile <jamilesphesihle99@gmail.com>
2023-01-01 22:46:58 +0200

    First Controller

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository)
+        {
+            this.regionRepository = regionRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = regionRepository.GetAll();
+
+            //Return DTO regions
+            var regionsDTO = new List<Models.DTO.Region>();
+            regions.ToList().ForEach(domainRegion =>
+            {
+                var regionDTO = new Models.DTO.Region()
+                {
+                    Id = domainRegion.Id,
+                    Name = domainRegion.Name,
+                    Code = domainRegion.Code,
+                    Area = domainRegion.Area,
+                    Lat = domainRegion.Lat,
+                    Long = domainRegion.Long,
+                    Population = domainRegion.Population
+                };
+                regionsDTO.Add(regionDTO);
+            });
+
+            return Ok(regionsDTO); //An OK response is a 200 HTTP response
+        }
+    }
+}
